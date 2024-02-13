45cd4d24b67e4e496ded3914f5dc515f6637947a
KenXD2001 <ashwinb708@gmail.com>
2024-02-13 22:30:34 +0530

    Added Frontend

---
+++ react_authentication_system/frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './Components/Login.jsx';
+import Signup from './Components/Signup.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
